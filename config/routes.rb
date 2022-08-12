e26c4dca2e729f7af37030a57bc95f0fa5a43902
tomonari-s <108882451+tomonari-s@users.noreply.github.com>
2022-08-12 22:25:20 +0000

    ファイルの記述・CRUDの実装0813

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  root to: 'homes#top'
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'homes#top'
+  get "homes/about" => "homes#about", as: "about"
+  resources :books, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    
+    resources :book_comments, only: [:create, :destroy]
+  end
   
-   resources :books, only: [:new, :index, :show]
-   
-   resources :users, only: [:show, :edit, :update]
-  
+  resources :users, only: [:show, :edit, :update]
 end
--- db/migrate/20220811115636_create_books.rb
+++ db/migrate/20220811115636_create_books.rb
@@ -2,7 +2,8 @@ class CreateBooks < ActiveRecord::Migration[6.1]
   def change
     create_table :books do |t|
       t.string :title
-      t.string :body
+      t.text :body
+      t.string :user_id
 
       t.timestamps
     end
