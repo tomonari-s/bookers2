5425aeeb178a4218724f6983cfc3f4f2e141bf14
tomonari-s <108882451+tomonari-s@users.noreply.github.com>
2022-08-14 00:48:44 +0000

    [start]sign upページの編集0814

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,17 @@
 class ApplicationController < ActionController::Base
+  
+  skip_before_action :verify_authenticity_token
+  
   before_action :authenticate_user!, except: [:top]
   
   before_action :configure_permitted_parameters, if: :devise_controller?
   
   def after_sign_in_path_for(resource)
-    about_path
+    user_path(current_user)
   end
   
   def after_sign_out_path_for(resource)
-    about_path
+    root_path
   end
 
   protected
@@ -16,4 +19,6 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
+  
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, only: [:show, :edit, :update]
   root to: 'homes#top'
+  
   get "homes/about" => "homes#about", as: "about"
   resources :books, only: [:new, :create, :index, :show, :destroy] do
     resource :favorites, only: [:create, :destroy]
@@ -8,5 +10,5 @@ Rails.application.routes.draw do
     resources :book_comments, only: [:create, :destroy]
   end
   
-  resources :users, only: [:show, :edit, :update]
+  
 end
