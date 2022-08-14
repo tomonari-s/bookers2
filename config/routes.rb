72eb3fd796f14b26383f28f864a6c609f484eae9
tomonari-s <108882451+tomonari-s@users.noreply.github.com>
2022-08-14 06:08:46 +0000

    [start]User info0814

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show, :edit, :update]
   root to: 'homes#top'
-  
   get "homes/about" => "homes#about", as: "about"
+  
   resources :books, only: [:new, :create, :index, :show, :destroy] do
     resource :favorites, only: [:create, :destroy]
     
