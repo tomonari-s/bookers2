5d328c2f75e43f757866ebf2fbf6ef82924fb219
tomonari-s <108882451+tomonari-s@users.noreply.github.com>
2022-08-17 13:30:03 +0000

    _list修正0817

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   
   skip_before_action :verify_authenticity_token
   
-  before_action :authenticate_user!, except: [:top]
+  before_action :authenticate_user!, except: [:top, :about]
   
   before_action :configure_permitted_parameters, if: :devise_controller?
   
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :edit, :update]
   root to: 'homes#top'
   get "homes/about" => "homes#about", as: "about"
-  
+  get "users/:id/edit" => "users#edit"
   resources :books, only: [:new, :create, :index, :show, :destroy] do
     resource :favorites, only: [:create, :destroy]
     
